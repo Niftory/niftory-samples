341c5684a40879483696c9620e74f38317c408ff
Anir Agarwal <agarwal.anir@gmail.com>
2022-07-26 21:38:09 -0700

    Revert "Add API Key and Copy Token into Header"
    This reverts commit fccc5e5b4815b475e03ec580186e6874ace4d1d7.

--- basic-app/components/AppHeader.tsx
+++ basic-app/components/AppHeader.tsx
@@ -1,44 +1,18 @@
-import {
-  VStack,
-  Heading,
-  Button,
-  Text,
-  HStack,
-  useClipboard,
-} from "@chakra-ui/react";
+import { VStack, Heading, Button, Text, HStack } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 
 import { signOutUser } from "./SignOutUser";
 import { useSession } from "next-auth/react";
-import React, { useEffect } from "react";
 
 export function AppHeader() {
-  const [value, setValue] = React.useState("");
-  const { onCopy } = useClipboard(value);
   const { data: session } = useSession();
   const router = useRouter();
 
-  React.useEffect(() => {
-    setValue(session?.authToken?.toString());
-  }, [session.authToken]);
-
   return (
-    <VStack textColor="white" mb="3vh">
+    <VStack textColor="white" mb = '3vh'>
       <Heading>Sample App: Logged In</Heading>
-      <Text>
-        User: {session?.user?.name} ({session?.user?.email})
-      </Text>
-      <Text> X-Niftory-API-Key: </Text>
-      <Text noOfLines={3} maxW="lg">
-        {process.env.NEXT_PUBLIC_API_KEY}
-      </Text>
-      <HStack>
-        <Text>Authorization</Text>
-        <Button colorScheme="gray" color="black" onClick={onCopy} size="sm">
-          Copy Value
-        </Button>
-      </HStack>
-
+      <Text> Name: {session?.user?.name} </Text>
+      <Text> Email: {session?.user?.email} </Text>
       <HStack>
         <Button
           colorScheme="blue"
