3cccb1bc7679cb1285097490c38ff896c5fa48d4
Udbhav Agarwal <udbhav.agarwal7@gmail.com>
2022-08-08 13:52:00 +0530

    updated nfts query for transfer

--- basic-app/pages/api/nft/[nftModelId]/transfer.ts
+++ basic-app/pages/api/nft/[nftModelId]/transfer.ts
@@ -50,7 +50,7 @@ const handler: NextApiHandler = async (req, res) => {
     nftModelId: nftModelId as string,
   })
 
-  if (userNftsResponse.nfts.length > 0) {
+  if (userNftsResponse.nfts?.items?.length > 0) {
     res.status(400).send("You already have an NFT from this model")
     return
   }
