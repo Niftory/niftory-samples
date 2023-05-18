ee45fe034a8f93941a0c1af093b89b27cd108146
Rojan Maharjan <roseonmhr@gmail.com>
2023-05-18 12:45:47 +0545

    fix: remove hard coded env

--- mintme/src/graphql/niftoryClientProvider.tsx
+++ mintme/src/graphql/niftoryClientProvider.tsx
@@ -1,5 +1,5 @@
 import { useAuthContext } from "hooks/useAuthContext"
-import { NiftoryClient, NiftoryProvider } from "@niftory/sdk"
+import { EnvironmentName, NiftoryClient, NiftoryProvider } from "@niftory/sdk"
 import { useMemo } from "react"
 
 export const NiftoryClientProvider = ({ children }) => {
@@ -7,7 +7,7 @@ export const NiftoryClientProvider = ({ children }) => {
 
   const client = useMemo(() => {
     return new NiftoryClient({
-      environmentName: "dev",
+      environmentName: process.env.NEXT_PUBLIC_BLOCKCHAIN_ENV as EnvironmentName,
       authToken: session?.authToken ?? "",
       appId: process.env.NEXT_PUBLIC_CLIENT_ID,
       apiKey: process.env.NEXT_PUBLIC_API_KEY,
