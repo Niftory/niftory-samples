1aae71eba40b780a488bced68ef22c1e3aea7693
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2023-08-01 09:10:38 -0400

    remove replace helper

--- walletless-onboarding/src/hooks/hybridCustody/useAddAccountMultiSign.ts
+++ walletless-onboarding/src/hooks/hybridCustody/useAddAccountMultiSign.ts
@@ -1,7 +1,6 @@
 import { useCallback } from "react"
 import ADD_ACCOUNT_MULTI_SIGN from "../../../cadence/transactions/hybrid-custody/add_account_multi_sign.cdc"
 import {NiftoryClient} from "@niftory/sdk"
-import { replaceCadencePlaceholders } from "utils/replaceCadencePlaceholders"
 
 export const useAddAccountMultisign = (fcl: any, niftoryClient: NiftoryClient) => {
   const signTransaction = useCallback(async (transaction: string, address: string) => {
@@ -11,9 +10,9 @@ export const useAddAccountMultisign = (fcl: any, niftoryClient: NiftoryClient) =
   const addAccountMultiSign = async ({ childAddress }) => {
     const signerKeyId = 0
     const { name: nftContractName } = await niftoryClient.getContract()
-    const cadence = replaceCadencePlaceholders(ADD_ACCOUNT_MULTI_SIGN, {CONTRACT_NAME: nftContractName })
+    // replace contract name placeholders in multisign transaciton
+    const cadence = ADD_ACCOUNT_MULTI_SIGN.replace(`<CONTRACT_NAME>`, nftContractName)
     const tx = await fcl.mutate({
-      // resolveCadenceImports converts explicit imports for Niftory sign operation
       cadence,
       args: (arg, t) => [
         arg(childAddress, t.Address),
--- walletless-onboarding/src/utils/replaceCadencePlaceholders.ts
+++
@@ -1,8 +0,0 @@
-export const replaceCadencePlaceholders = (cadence: String, map: Record<string, string>): String => {
-  const result = Object.entries(map).reduce((acc, [key, replaceValue]) => {
-    const searchValue = new RegExp(`<${key}>`, "g")
-    return acc.replace(searchValue, replaceValue)
-  }, cadence)
-
-  return result
-}
