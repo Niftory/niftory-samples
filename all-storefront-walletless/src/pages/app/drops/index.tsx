f4c8428bd1376af3ce401514da05f2c614bdda6c
Mark Reese <marksreese@gmail.com>
2023-08-02 15:57:56 -0700

    add drops and drop details pages

---
+++ all-storefront-walletless/src/pages/app/drops/index.tsx
@@ -0,0 +1,32 @@
+import { Center, Spinner } from "@chakra-ui/react"
+import React from "react"
+import { useQuery } from "urql"
+
+import { NftListingsDocument, NftListingsQuery } from "../../../../generated/graphql"
+import AppLayout from "../../../components/AppLayout"
+import { DropsGrid } from "../../../components/drops/DropsGrid"
+import { UpcomingDrop } from "../../../ui/marketing"
+import { SectionHeader } from "../../../ui/SectionHeader"
+
+const DropsPage = () => {
+  const [nftListingsQueryResponse] = useQuery<NftListingsQuery>({
+    query: NftListingsDocument,
+    variables: { maxResults: "100", filter: { state: "active" } },
+  })
+
+  const nftListings = nftListingsQueryResponse.data?.nftListings?.items
+
+  return (
+    <AppLayout>
+      <SectionHeader text="Get A Drop" />
+      {!nftListings && (
+        <Center>
+          <Spinner mt="16" color="white" size="lg" />
+        </Center>
+      )}
+      <DropsGrid nftListings={nftListings} /> <UpcomingDrop />
+    </AppLayout>
+  )
+}
+
+export default DropsPage
