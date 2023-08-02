19772506f9bcf097e6ae80a2ce25db3a4267a77b
Mark Reese <marksreese@gmail.com>
2023-08-02 15:56:27 -0700

    update nav bar to have drops and login/acc links

--- all-storefront-walletless/src/ui/Navbar/Nav.tsx
+++ all-storefront-walletless/src/ui/Navbar/Nav.tsx
@@ -1,4 +1,5 @@
 import {
+  Avatar,
   Link as ChakraLink,
   Heading,
 } from "@chakra-ui/react"
@@ -7,27 +8,60 @@ import * as React from "react"
 import { Navbar as NiftoryNavbar } from "./Navbar"
 
 import { BsDiscord } from "react-icons/bs"
+import { useAuthContext } from "hooks/useAuthContext"
+
 
 export const Navbar = ({ onOpen }) => {
+  const { session } = useAuthContext()
 
 
   const menuItems = React.useMemo(() => {
+    const items = [{
+      title: "Drops",
+      href: "/app/drops"
+    }] as Array<Object>
 
-    return [
-      {
-        href: "https://discord.gg/QAgDQXUGsU",
-        component: (
-          <ChakraLink
-            href="https://discord.gg/QAgDQXUGsU"
-            target="_blank"
-          >
-            <BsDiscord size="1.5rem" color="#2D3436" />
-          </ChakraLink>
-        ),
-      },
-    ]
+    if (!session) {
+      items.push(
+        {
+          title: "Log In",
+          href: "/login"
+        })
+    }
+    else {
+      items.push(
+        {
+          href: "/app/account",
+          component: (
+            <Link href="/app/account" passHref>
+              <ChakraLink>
+                <Avatar size="sm" ></Avatar>
+              </ChakraLink>
+            </Link>
+          ),
+          hideOnMobile: true,
+        },
+        {
+          title: "Account",
+          href: "/app/account",
+          hideOnWeb: true,
+        },
+      )
+    }
+    items.push({
+      href: "https://discord.gg/QAgDQXUGsU",
+      component: (
+        <ChakraLink
+          href="https://discord.gg/QAgDQXUGsU"
+          target="_blank"
+        >
+          <BsDiscord size="1.5rem" color="#2D3436" />
+        </ChakraLink>
+      ),
+    })
+    return items
 
-  }, [])
+  }, [session])
 
   return (
     <>
