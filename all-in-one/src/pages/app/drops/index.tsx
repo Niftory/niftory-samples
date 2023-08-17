d8a42297b456829caee8a1afe8fbba920c4c4593
Mark Reese <marksreese@gmail.com>
2023-08-17 11:33:37 -0700

    use hooks from sdk in drops/index

--- all-in-one/src/pages/app/drops/index.tsx
+++ all-in-one/src/pages/app/drops/index.tsx
@@ -1,6 +1,5 @@
-import { Center, Spinner } from "@chakra-ui/react"
-import { useQuery } from "urql"
-import { NftListingsDocument, NftListingsQuery } from "@niftory/sdk"
+import { Box, Center, Spinner } from "@chakra-ui/react"
+import { useNftModelsQuery, useNftModelQuery } from "@niftory/sdk/react"
 
 import AppLayout from "../../../components/AppLayout"
 import { DropsGrid } from "../../../components/drops/DropsGrid"
@@ -8,22 +7,31 @@ import { UpcomingDrop } from "../../../components/drops/UpcomingDrop"
 import { SectionHeader } from "../../../ui/SectionHeader"
 
 const DropsPage = () => {
-  const [nftListingsQueryResponse] = useQuery<NftListingsQuery>({
-    query: NftListingsDocument,
-    variables: { maxResults: "100", filter: { state: "active" } },
-  })
+  // const [nftListingsQueryResponse] = useQuery<NftListingsQuery>({
+  //   query: NftListingsDocument,
+  //   variables: { maxResults: "100", filter: { state: "active" } },
+  // })
 
-  const nftListings = nftListingsQueryResponse.data?.nftListings?.items
+  const [{data, fetching: isFetching}] = useNftModelsQuery()
+
+  const nftModels = data?.nftModels.items
+  const modelIds = []
+  nftModels?.forEach((currentModel) => {
+    modelIds.push(currentModel.id)
+  })
 
   return (
     <AppLayout>
-      <SectionHeader ml="16" standardText="List your drops here!" />
-      {!nftListings && (
+      <SectionHeader ml="16" standardText="List your drops here!"/>
+      {isFetching && (
         <Center>
           <Spinner mt="16" color="white" size="lg" />
         </Center>
       )}
-      <DropsGrid nftListings={nftListings} />
+      {modelIds.length !== 0
+        ? (<DropsGrid nftListings={modelIds} />)
+        : (<Box textColor="white" >No drops available yet</Box>)
+      }
     </AppLayout>
   )
 }
