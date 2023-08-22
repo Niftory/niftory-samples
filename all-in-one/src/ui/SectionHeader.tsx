6a6d0e0e50aefb2bd0418c6476565d5d743491d5
Mark Reese <marksreese@gmail.com>
2023-08-22 08:32:22 -0700

    update SectionHeader text color with theme

--- all-in-one/src/ui/SectionHeader.tsx
+++ all-in-one/src/ui/SectionHeader.tsx
@@ -21,7 +21,7 @@ export const SectionHeader = (props: SectionHeaderProps) => {
     >
       {standardText}{" "}
       {highlightedText && (
-        <Box as="mark" color="page.accent" bg="transparent">
+        <Box as="mark" color="page.text" bg="transparent">
           {highlightedText}{" "}
         </Box>
       )}
