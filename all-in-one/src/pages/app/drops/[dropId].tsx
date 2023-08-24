5e142cd40108444bd2bff0f99c96556eee6a870a
Mark Reese <marksreese@gmail.com>
2023-08-24 16:13:50 -0700

    grab id from url string and pass data from sdk query

--- all-in-one/src/pages/app/drops/[dropId].tsx
+++ all-in-one/src/pages/app/drops/[dropId].tsx
@@ -1,15 +1,23 @@
 import { Box } from "@chakra-ui/react"
 
-import AppLayout from "../../../components/AppLayout"
-import { DropDetailPage } from "../../../components/drops/DropDetailPage"
+import AppLayout from "components/AppLayout"
+import { NFTModelDetail } from "components/drops/NFTModelDetail"
+import { useNftModelQuery } from "@niftory/sdk/react"
+import { useRouter } from "next/router"
 
-const ProductDropPage = () => {
+const DropDetailPage = () => {
+  const router = useRouter()
+  const nftModelId = router.query["nftModelId"]?.toString()
+
+  const [modelResponse, _] = useNftModelQuery({variables: {id: nftModelId }})
+  const nftModel = modelResponse.data.nftModel
+  
   return (
     <AppLayout>
       <Box bg="page.background" pos="relative" width="100%" height="80px"></Box>
-      <DropDetailPage />
+      <NFTModelDetail id={nftModel.id}  metadata={nftModel.metadata} />
     </AppLayout>
   )
 }
 
-export default ProductDropPage
+export default DropDetailPage
