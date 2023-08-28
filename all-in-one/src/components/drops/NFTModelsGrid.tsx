59cf67dd06d8ad76707d33d55788372b1dc7f7a5
Mark Reese <marksreese@gmail.com>
2023-08-28 14:05:18 -0700

    add bottom padding to drops grid

--- all-in-one/src/components/drops/NFTModelsGrid.tsx
+++ all-in-one/src/components/drops/NFTModelsGrid.tsx
@@ -3,7 +3,7 @@ import { SimpleGrid } from '@chakra-ui/react';
 import { NFTModelCard } from './NFTModelCard';
 
 export const NFTModelsGrid = ({ nftModels }) => (
-  <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 4 }} gap="8">
+  <SimpleGrid pb={32} columns={{ base: 1, sm: 2, md: 3, lg: 4 }} gap="8">
     {nftModels?.map((nftModel) => {
       return (
         <NFTModelCard key={nftModel?.id} nftModel={nftModel} clickUrl={`drops/${nftModel?.id}`} />
