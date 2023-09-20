edae1e181838e776535d8532d2dd7a26261bb56d
Mark Reese <marksreese@gmail.com>
2023-09-20 00:39:59 -0700

    change ParentWallet to button
    attach to WalletCard

---
+++ all-in-one/src/ui/HybridCustody/ParentWalletButton.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+import * as fcl from "@onflow/fcl"
+import {
+  Button,
+  SimpleGrid,
+  GridItem,
+  Text,
+  Box,
+  Stack,
+  Heading,
+  StackDivider,
+} from "@chakra-ui/react"
+
+import "../../lib/fcl/config"
+import { Wallet, useNiftoryClient } from "@niftory/sdk/react"
+
+import { useAddAccountMultisign } from "hooks/hybridCustody/useAddAccountMultiSign"
+
+interface Props {
+  wallet: Wallet
+}
+
+export const ParentWalletButton = ({ wallet }: Props) => {
+  const niftoryClient = useNiftoryClient()
+
+  const { addAccountMultiSign } = useAddAccountMultisign(fcl, niftoryClient)
+
+  return (
+    <Button px="4" py="2" size="sm" onClick={() => addAccountMultiSign({childAddress: wallet.address})} color="black">
+      Link wallet
+    </Button>
+
+  )
+}
